39c014879af202612831979df5dff28a984159a5
MrunmayRajgire <113378987+MrunmayRajgire@users.noreply.github.com>
2025-01-14 19:09:55 +0530

    completed 3.5

--- backend/3.5 Secrets Project/index.js
+++ backend/3.5 Secrets Project/index.js
@@ -14,3 +14,31 @@ let authorised = false;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
+function check(req, res, next) {
+  const password = req.body["password"];
+  if (password === "ILoveProgramming") {
+    authorised = true;
+  }
+  next();
+}
+
+app.use(check);
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/public/index.html')");
+});
+
+app.post("/check", (req, res) => {
+    if(authorised)
+    {
+        res.sendFile(__dirname + "/public/secret.html");
+    }
+    else
+    {
+        res.redirect('/');
+    }
+});
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+});
