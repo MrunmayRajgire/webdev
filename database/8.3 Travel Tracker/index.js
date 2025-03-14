c38ca4257c6dd354d42796deef20e24fa366938c
MrunmayRajgire <113378987+MrunmayRajgire@users.noreply.github.com>
2025-03-14 12:52:33 +0530

    8.3_2

--- database/8.3 Travel Tracker/index.js
+++ database/8.3 Travel Tracker/index.js
@@ -18,16 +18,38 @@ const port = 3000;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-app.get("/", async (req, res) => {
-  //Write your code here.
+async function checkVisited() {
   const result = await db.query("SELECT country_code FROM visited_countries");
-  let countries = [];
-  result.rows.forEach((country) => {
+  let countries=[];
+  result.rows.forEach((country)=>{
     countries.push(country.country_code);
   });
-  console.log(result.rows);
+  return countries;
+}
+
+app.get("/", async (req, res) => {
+  //Write your code here.
+  const countries = await checkVisited();
   res.render("index.ejs", { countries: countries, total: countries.length });
-  db.end();
+});
+
+app.post("/add", async (req, res) => {
+  const input = req.body["country"];
+
+  const result = await db.query(
+    "SELECT country_code FROM countries WHERE country_name = $1",
+    [input]
+  );
+  console.log(result.rows);
+  if (result.rows.length !== 0) {
+    const data = result.rows[0];
+    const countryCode = data.country_code;
+
+    await db.query("INSERT INTO visited_countries (country_code) VALUES ($1)", [
+      countryCode,
+    ]);
+    res.redirect("/");
+  }
 });
 
 app.listen(port, () => {
